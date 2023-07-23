a9093e3a0e83fa0bf0946f4c20fec1f7388914f6
Coolbossco <87975966+Coolbossco@users.noreply.github.com>
2023-07-23 16:18:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,39 +4,44 @@ const Discord = require('discord.js');
 const Client = require('./client/client.js');
 const config = require('./config.json');
 const glob = require('glob');
+const readline = require('readline');
 const { TicketManager } = require('discord-tickets');
 
 const client = new Client();
 client.commands = new Discord.Collection();
-
-fs.readdir("./events/", (_err, files) => {
-    files.forEach((file) => {
-        if (!file.endsWith(".js")) return;
-        const event = require(`./events/${file}`);
-        let eventName = file.split(".")[0];
-        console.log(`👌 Event loaded: ${eventName}`);
-        console.log(`Event is a ${typeof event}`);
-        if (typeof event !== 'function') {
-            console.log(`Event file: ${file}`);
-            console.log(event);
-        } else {
-            client.on(eventName, event.bind(null, client));
-            delete require.cache[require.resolve(`./events/${file}`)];
-        }
-    });
-});
-
-client.commands = new Discord.Collection();
 client.ticketManager = new TicketManager(client, config.ticketManager);
 
-const commandFiles = glob.sync("./commands/**/*.*");
-commandFiles.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    let props = require(path.resolve(file));
-    let commandName = path.basename(file, '.js');
-    client.commands.set(commandName, props);
-    console.log(`👌 Command loaded: ${commandName}`);
-});
+// Function to load events
+const loadEvents = () => {
+    fs.readdir("./events/", (_err, files) => {
+        files.forEach((file) => {
+            if (!file.endsWith(".js")) return;
+            delete require.cache[require.resolve(`./events/${file}`)];
+            const event = require(`./events/${file}`);
+            let eventName = file.split(".")[0];
+            console.log(`👌 Event loaded: ${eventName}`);
+            client.on(eventName, event.bind(null, client));
+        });
+    });
+}
+
+// Function to load commands
+const loadCommands = () => {
+    client.commands.clear();
+    const commandFiles = glob.sync("./commands/**/*.*");
+    commandFiles.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        delete require.cache[require.resolve(path.resolve(file))];
+        let props = require(path.resolve(file));
+        let commandName = path.basename(file, '.js');
+        client.commands.set(commandName, props);
+        console.log(`👌 Command loaded: ${commandName}`);
+    });
+}
+
+// Load events and commands
+loadEvents();
+loadCommands();
 
 client.once('ready', () => {
     console.log('Ready!');
@@ -50,7 +55,6 @@ client.on('message', message => {
 
     if (!client.commands.has(command)) return;
 
-    // Log the command usage
     client.logCommand(message);
 
     try {
@@ -62,3 +66,22 @@ client.on('message', message => {
 });
 
 client.login(config.token);
+
+// Terminal input for refreshing commands and events
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (input) => {
+    if (input.toLowerCase() === 'refresh') {
+        console.log('Refreshing commands and events...');
+        try {
+            loadEvents();
+            loadCommands();
+            console.log('Refreshed successfully.');
+        } catch (error) {
+            console.error(`Error occurred during refreshing: ${error}`);
+        }
+    }
+});
